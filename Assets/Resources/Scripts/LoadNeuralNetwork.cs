e3cdeb247db7ebf38ea5891b77acb1a1a4a09794
lodo744 <55733870+lodo744@users.noreply.github.com>
2020-08-27 21:48:57 +0200

    Add LoadNeuralNetwork, TestNeuralNetwork and Utils

---
+++ Assets/Resources/Scripts/LoadNeuralNetwork.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Barracuda;
+using UnityEngine;
+
+public class LoadNeuralNetwork : MonoBehaviour
+{
+    private static NNModel modelAsset = (NNModel)Resources.Load("NeuralNetwork/gender");
+    public static string[] label = { "Man", "Woman" };
+    void Start()
+    {
+    }
+    void Update()
+    {
+    }
+
+    public static IWorker getModel()
+    {
+        Model m_RuntimeModel = ModelLoader.Load(model: modelAsset);
+        IWorker m_Worker = WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, m_RuntimeModel);
+        return m_Worker;
+    }
+}
---
+++ Assets/Resources/Scripts/Utils.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Barracuda;
+using UnityEngine;
+
+public class Utils : MonoBehaviour
+{
+    public const int IMAGE_SIZE = 64;
+    public Texture2D picture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color32[] pic = picture.GetPixels32();
+        Debug.Log(pic.GetValue(0));
+        Tensor trasformato = TransformInputToTensor(picture, IMAGE_SIZE, IMAGE_SIZE);
+        Debug.Log(trasformato.DataToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Questa funzione trasforma l'immagine di input in un tensore di dimensione (1 x width x height x channels)
+    public static Tensor TransformInputToTensor(Texture2D texture, int width, int height)
+    {
+        Color[] colors = texture.GetPixels();
+        float[] results = new float[(width * height) * 3];
+        for (int i = 0; i < (width*height); i++)
+        {
+            results[i * 3] = colors[i].r;
+            results[i * 3 + 1] = colors[i].g;
+            results[i * 3 + 2] = colors[i].b;
+        }
+        return new Tensor(1, width, height, 3, results);
+    }
+}
